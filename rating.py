2e4b44fa708a6076df0a9f297c38a1a80fdce248
Frozen-byte <Frozen_byte@gmx.de>
2013-05-10 17:03:35 +0200

    Update rating.py

--- rating.py
+++ rating.py
@@ -12,29 +12,15 @@ banshee = dbus.Interface(banshee, 'org.bansheeproject.Banshee.PlayerEngine')
 #extract userrating
 userRating = int(banshee.GetRating())
 
-#some converting
-if userRating == 0.2:
-  rating = 1
-elif userRating == 0.4:
-	rating = 2
-elif userRating == 0.6:
-	rating = 3
-elif userRating == 0.8:
-	rating = 4
-elif userRating == 1:
-	rating = 5
-else:
-	rating = 0
-
 #modify the rating
 if sys.argv[1] == 'inc':
-	if rating < 5:
-		rating += 1
-		subprocess.call( ['banshee', '--set-rating='+str(rating)])
+	if userRating < 5:
+		userRating += 1
+		subprocess.call( ['banshee', '--set-rating='+str(userRating)])
 elif sys.argv[1] == 'dec':
-	if rating > 0:
-		rating -= 1
-		subprocess.call( ['banshee', '--set-rating='+str(rating)])
+	if userRating > 0:
+		userRating -= 1
+		subprocess.call( ['banshee', '--set-rating='+str(userRating)])
 elif sys.argv[1] == 'res':
 	subprocess.call( ['banshee', '--set-rating=3'])
 elif sys.argv[1] == 'min':
